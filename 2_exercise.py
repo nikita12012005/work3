e0708114050c5b4549dc12d44fd17bbce1e2bd3c
Nikita <kruknikitron@gmail.com>
2023-05-13 01:33:57 +0300

    Initial commit 2

--- 2_exercise.py
+++ 2_exercise.py
@@ -1,8 +1,3 @@
-odd = [1, 3, 5, 7]
-even = [2, 4, 6, 8]
-
-for index, value in zip(odd, even):
-    print(f'index: {index}, value: {value}')
 #2_exercise
 multiplication_way_one= 2*2
 print(multiplication_way_one)
